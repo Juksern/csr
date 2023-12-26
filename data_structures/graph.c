4fb62c594c73c4e9129bfb7ac5cf737bd0171dee
snoooo@bitbucket.org <gothicqwq@gmail.com>
2023-12-26 23:27:36 +0100

    added freeing of memory to the graphs

--- data_structures/graph.c
+++ data_structures/graph.c
@@ -18,19 +18,30 @@ struct node_t* create_node(int data, struct node_t* left, struct node_t* right) 
 	return temp;
 }
 
-void graph_traverse(struct node_t* node) {
-	printf("%d", node->data);
+void node_traverse(struct node_t* node) {
+	printf("%d\n", node->data);
 
 	if (node->left)
-		graph_traverse(node->left);
+		node_traverse(node->left);
 
 	if (node->right)
-		graph_traverse(node->right);
+		node_traverse(node->right);
+}
+
+void node_free(struct node_t* node) {
+	if (node->left)
+		node_free(node->left);
+
+	if (node->right)
+		node_free(node->right);
+
+	printf("freeing -> %d\n", node->data);
+	free(node);
 }
 
 int main() {
 	struct node_t* a = create_node(1, create_node(2, NULL, create_node(4, NULL, NULL)), create_node(3, NULL, NULL));
-
-	graph_traverse(a);
+	node_traverse(a);
+	node_free(a);
 	return 0;
 }
